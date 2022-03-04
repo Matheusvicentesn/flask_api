3cabff91d81cadffd229c495d6f90b18dc45f482
root <root@pop-os.localdomain>
2022-03-04 16:07:00 -0300

    first commit

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+
+USER = os.environ.get("USER")
+PASSWORD = os.environ.get("PASSWORD")
+DATABASE = os.environ.get("DATABASE")
+JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+PORT = os.environ.get("PORT")
+HOST = os.environ.get("HOST")
+
+
